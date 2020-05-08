97d3bf9f44e16a06892af4cbe0c2b10a54faa161
Christian Nascimento <cnascimentobr@gmail.com>
2020-05-08 11:37:59 -0300

    Add interface for user

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useObserver } from "mobx-react";
+import { StoreContext } from "../App";
 import TaskForm from "./Form";
 
 export default function Header() {
-  return (
+  const { UserStore: store } = useContext(StoreContext);
+  return useObserver(() => (
     <header className="App-header">
       <h1>POMODX</h1>
       <TaskForm />
+      <div className="avatar">
+        <img src={store?.currentUser.avatar} alt={store.currentUser.name} />
+      </div>
     </header>
-  );
+  ));
 }
--- src/store/users.tsx
+++ src/store/users.tsx
@@ -1,8 +1,15 @@
-import { decorate, observable } from "mobx";
+import { decorate, observable, computed } from "mobx";
+
+export interface User {
+  name: string;
+  email: string;
+  avatar: string;
+}
 class Store {
-  user = {
+  user: User = {
     name: "Christian Nascimento",
     email: "cnascimentobr@gmail.com",
+    avatar: "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50",
   };
 
   get currentUser() {
@@ -12,6 +19,7 @@ class Store {
 
 decorate(Store, {
   user: observable,
+  currentUser: computed,
 });
 
 export const UserStore = new Store();
